e7d5989817987e8705e0d7592410bc0c06c4ab69
Joel <japollodev@gmail.com>
2026-09-22 14:16:18 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     while sentinel :
         sys.stdout.write("$ ")
         command = input()
-        index = command.find(" ") + 1
+        index = command.find(" ")
         initial_command = command[:index]
         if initial_command not in command_list:
             print(f"{initial_command}: command not found")
