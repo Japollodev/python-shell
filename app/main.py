7b6a36ea388cefc840b2c6a3d8bb91883608aa84
Joel <japollodev@gmail.com>
2026-09-22 14:12:21 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,17 +3,22 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    command_list = {}
+    command_list = ['echo', 'exit']
     sentinel = True
 
     while sentinel :
         sys.stdout.write("$ ")
         command = input()
+        if command not in command_list:
+            print(f"{command}: command not found")
         if command == "exit" :
             sentinel = False
             break
-        if command not in command_list:
-            print(f"{command}: command not found")
+        if command.startswith("echo ") :
+            index = command.find(" ") + 1
+            output = command[index:]
+            print(f"{command}: {output}")
+
 
 if __name__ == "__main__":
     main()
