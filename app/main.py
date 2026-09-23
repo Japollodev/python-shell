ae746b55439d7da165c198b78e37f9f8f3641667
Joel <japollodev@gmail.com>
2026-09-23 12:52:46 -0400

    Update type functionality to search for command files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
@@ -32,7 +32,31 @@ def main():
                 if second_command in command_list:
                     print(f"{second_command} is a shell builtin")
                 else:
-                    print(f"{second_command}: not found")
+                    """
+                    If the command is not a builtin, your shell must go through every directory in PATH. For each directory:
+                        - Check if a file with the command name exists.
+                        - Check if the file has execute permissions.
+                        - If the file exists and has execute permissions, print <command> is <full_path> and stop.
+                        - If the file exists but lacks execute permissions, skip it and continue to the next directory.
+                    """
+
+                    env_path = os.environ.get("PATH", '')
+                    cmd_directories = env_path.split(os.pathsep)
+
+                    for directory in cmd_directories:
+                        queried_path = os.path.join(directory, second_command)
+
+                        # Determine if query path for command file exists and has permissions
+                        cmd_file_exists = os.path.exists(queried_path)
+                        cmd_file_has_execute_permissions = os.access(queried_path, os.X_OK)
+                        if cmd_file_exists and cmd_file_has_execute_permissions:
+                            cmd_path = queried_path
+                            print(f"{second_command} is {cmd_path}")
+                            break
+                        elif cmd_file_exists and not cmd_file_has_execute_permissions:
+                            continue
+                    else:
+                        print(f"{second_command}: not found")
 
 
         # Notify  user if the command is not found in acceptable list
